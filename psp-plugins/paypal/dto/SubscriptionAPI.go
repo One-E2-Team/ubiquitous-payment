eb374a46bfc452163a342ffbf54df164decf6f88
Igor Šikuljak <41073725+FmasterofU@users.noreply.github.com>
2021-12-08 23:36:36 +0100

    subscription def init for paypal

--- psp-plugins/paypal/dto/SubscriptionAPI.go
+++ psp-plugins/paypal/dto/SubscriptionAPI.go
@@ -5,3 +5,17 @@ type Subscription struct {
 	InvoiceId          string             `json:"custom_id"`
 	ApplicationContext ApplicationContext `json:"application_context"`
 }
+
+func (s *Subscription) DefaultInit(planId string, pspOrderId string, webshop string, returnUrl string, cancelUrl string) Subscription {
+	s.PlanId = planId
+	s.InvoiceId = pspOrderId
+	s.ApplicationContext = ApplicationContext{
+		BrandName:   webshop,
+		Locale:      "en-RS",
+		LandingPage: Login,
+		UserAction:  PayNow,
+		ReturnUrl:   returnUrl,
+		CancelUrl:   cancelUrl,
+	}
+	return *s
+}
