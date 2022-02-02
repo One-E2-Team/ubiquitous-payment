d428cd3d8e4712fcc8f8a3b07824a8081bae5657
hi-imAndy <andrej.1998@uns.ac.rs>
2022-02-02 19:47:40 +0100

    webshop front - CRUD for accounts

--- webshop/handler/accountHandler.go
+++ webshop/handler/accountHandler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 	"ubiquitous-payment/util"
+	"ubiquitous-payment/webshop/model"
 )
 
 func (handler *Handler) GetAccountsByPaymentType(w http.ResponseWriter, r *http.Request) {
@@ -14,4 +16,45 @@ func (handler *Handler) GetAccountsByPaymentType(w http.ResponseWriter, r *http.
 		return
 	}
 	util.MarshalResult(w, ret)
+}
+
+func (handler *Handler) UpdateAccount(w http.ResponseWriter, r *http.Request) {
+	id := util.String2Uint(util.GetPathVariable(r, "id"))
+	var account model.Account
+	err := json.NewDecoder(r.Body).Decode(&account)
+	if err != nil{
+		util.HandleErrorInHandler(err, w, loggingClass+"UpdateAccount", loggingService)
+	}
+	err = handler.WSService.UpdateAccount(&account, id)
+	if err != nil {
+		util.HandleErrorInHandler(err, w, loggingClass+"UpdateAccount", loggingService)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (handler *Handler) CreateAccount(w http.ResponseWriter, r *http.Request) {
+	loggedUserId := util.GetLoggedUserIDFromToken(r)
+	var account model.Account
+	err := json.NewDecoder(r.Body).Decode(&account)
+	if err != nil{
+		util.HandleErrorInHandler(err, w, loggingClass+"CreateAccount", loggingService)
+	}
+	account.ProfileId = loggedUserId
+	err = handler.WSService.CreateAccount(&account)
+	if err != nil {
+		util.HandleErrorInHandler(err, w, loggingClass+"CreateAccount", loggingService)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (handler *Handler) DeleteAccount(w http.ResponseWriter, r *http.Request) {
+	id := util.String2Uint(util.GetPathVariable(r, "id"))
+	err := handler.WSService.DeleteAccount(id)
+	if err != nil {
+		util.HandleErrorInHandler(err, w, loggingClass+"DeleteAccount", loggingService)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
--- webshop/service/accountService.go
+++ webshop/service/accountService.go
@@ -1,6 +1,10 @@
 package service
 
-import "ubiquitous-payment/webshop/model"
+import (
+	"errors"
+	"strings"
+	"ubiquitous-payment/webshop/model"
+)
 
 func (service *Service) GetAccountsByPaymentName(name string, loggedUserId uint) ([]model.Account, error) {
 	paymentType, err := service.WSRepository.GetPaymentTypeByName(name)
@@ -8,4 +12,24 @@ func (service *Service) GetAccountsByPaymentName(name string, loggedUserId uint)
 		return nil, err
 	}
 	return service.WSRepository.GetAccountsByProfileIdAndPaymentType(loggedUserId, paymentType.ID)
+}
+
+func (service *Service) UpdateAccount(newAcc *model.Account, id uint) error {
+	 acc := service.WSRepository.GetAccountById(id)
+	 if acc == nil{
+	 	return errors.New("account does not exist")
+	 }
+	 acc.AccountID = newAcc.AccountID
+	 acc.Secret = newAcc.Secret
+	 return service.WSRepository.Update(acc)
+}
+
+func (service *Service) CreateAccount(account *model.Account) error {
+	account.AccountID = strings.Trim(account.AccountID, "\t")
+	account.Secret = strings.Trim(account.Secret, "\t")
+	return service.WSRepository.CreateAccount(account)
+}
+
+func (service *Service) DeleteAccount(id uint) error {
+	return service.WSRepository.DeleteAccount(id)
 }
